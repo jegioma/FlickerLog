1987e90d49b4ca830409a118f05a16aa2aef65d5
Jeancarlo Masciotti <jkiaba@protonmail.com>
2023-09-06 15:55:35 -0400

    mock up of landing page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from '@/components/header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Header/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
