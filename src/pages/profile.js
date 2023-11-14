18faf5e9780f0173dbc3c4d8ebbda1e66f344800
Jeancarlo Masciotti <jkiaba@protonmail.com>
2023-11-14 12:16:04 -0500

    Functionality added to watchlist button with view changes depending on user login

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,30 +1,26 @@
 import {
-  Box, SimpleGrid, Grid, GridItem, VStack, HStack, Stack, Text, Heading, Image, Card, IconButton,
-  useDisclosure,
+  Box, Grid, GridItem,
 } from '@chakra-ui/react';
-import { AddIcon } from '@chakra-ui/icons';
 import UserInfo from '@/components/userInfo';
-import CreateWatchList from '@/components/createWatchList';
 import {DisplayList} from '@/components/displayWatchlist';
 import { FriendList } from '@/components/friendsList';
 export default function Profile() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box height='100vh' width='90vw' padding='3rem'>
       <Grid templateColumns='repeat(2, 1fr)' gap={5} height='90%'>
         <GridItem>
-        <UserInfo />
+          <UserInfo />
         </GridItem>
+
         <GridItem>
           <DisplayList/>
         </GridItem>
-
+        
         <GridItem>
           <FriendList/>
         </GridItem>
       </Grid>
-      
     </Box>
   );
 }
